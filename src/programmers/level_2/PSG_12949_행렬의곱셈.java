188bf0dc0a0f15bed67d88400f5d725b1a74c49e
yongho9064 <dydgmldydgh@naver.com>
2024-11-12 03:54:56 +0900

    [프로그래머스/배열] 행렬의곱셈

--- src/programmers/level_2/PSG_12949_행렬의곱셈.java
+++ src/programmers/level_2/PSG_12949_행렬의곱셈.java
@@ -14,17 +14,11 @@ public class PSG_12949_행렬의곱셈 {
 
         int[][] answer = new int[arr1.length][arr2[0].length];
 
-        int var = 0;
-
-        int i, j, k;
-
-        for (i = 0; i < arr1.length; i++) {
-            for (j = 0; j < arr2[0].length; j++) {
-                for (k = 0; k < arr1[0].length; k++) {
-                    var += arr1[i][k] * arr2[k][j];
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2[0].length; j++) {
+                for (int k = 0; k < arr1[0].length; k++) {
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
                 }
-                answer[i][j] = var;
-                var = 0;
             }
         }
 
